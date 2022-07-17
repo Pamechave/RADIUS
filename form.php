86aecaacda0d22a5fec42d4a5c18c6460d84dc3c
Pamela Echave <pame_he@hotmail.com>
2022-07-17 18:09:35 -0300

    agregue pagina de agradecimiento para form

--- form.php
+++ form.php
@@ -1,20 +1,20 @@
 <?php
-$nombre = $_POST ["campo-nombre"];
-$telefono = $_POST ["campo-telefono"];
-$direccion = $_POST ["campo-dirección"];
-$mail = $_POST ["campo-mail"];
-$mensaje = $_POST ["text"];
+$nombre = $_POST ['campo-nombre'];
+$telefono = $_POST ['campo-telefono'];
+$direccion = $_POST ['campo-direccion'];
+$mail = $_POST ['campo-mail'];
+$mensaje = $_POST ['text'];
 
-$mensaje = "Este mensaje fue enviado por " , $nombre , ",/r/n";
-$mensaje .= "Su telefono es " , $telefono , ",/r/n";
-$mensaje .= "Su direccion es " , $direccion , ",/r/n";
-$mensaje .= "Su e-mail es " , $mail , ",/r/n";
-$mensaje .= "Mensaje " , $_POST["text"] , ",/r/n";
-$mensaje -= "Mensaje enviado el" , date(d,m,Y , time());
+$mensaje = "Este mensaje fue enviado por " . $nombre , ",/r/n";
+$mensaje .= "Su telefono es " . $telefono , ",/r/n";
+$mensaje .= "Su direccion es " . $direccion , ",/r/n";
+$mensaje .= "Su e-mail es ". $mail , ",/r/n";
+$mensaje .= "Mensaje " . $_POST["mensaje"] , ",/r/n";
+$mensaje .= "Mensaje enviado el". date('d,m,Y' , time());
 
-$para = "pame_he@hotmail.com";
+$para = 'pame_he@hotmail.com';
 $asunto = "Este mensaje fue enviado desde la web";
 
 mail($para, $asunto, utf8_decode($mensaje), $header)
-header("location:exito.html")
+header('location:exito.html')
 ?>
